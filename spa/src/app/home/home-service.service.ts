3e5152808403c2f466eb134d769f6d8a18fdb744
user16 <user16@PC3>
2022-01-16 22:32:23 +0300

    analytics

---
+++ spa/src/app/home/home-service.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+declare var gtag: any;
+declare var window: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeServiceService {
+
+  constructor() {
+    gtag('config', 'AW-10778531434');
+  }
+
+  public onChromeStore(url: string) {
+    this.send(url, 'AW-10778531434/l32dCMvr5ZADEOqszZMo');
+  }
+
+  public onEdgeStore(url: string) {
+    this.send(url, 'AW-10778531434/djQKCOae5pADEOqszZMo');
+  }
+
+  public onMobileDevices(): Observable<void> {
+    return new Observable((observer) => {
+      gtag('event', 'conversion', {
+        send_to: 'AW-10778531434/tXRwCJfooJEDEOqszZMo',
+        event_callback: () => {
+          observer.next();
+          observer.complete();
+        }
+      });
+    });
+  }
+
+  private send(url: string, tag: string) {
+    const callback = () => {
+      if (typeof (url) != 'undefined') {
+        window.location = url;
+      }
+    };
+    gtag('event', 'conversion', { send_to: tag, event_callback: callback });
+  }
+}
--- spa/src/app/home/home.component.ts
+++ spa/src/app/home/home.component.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 import { MatBottomSheet } from "@angular/material/bottom-sheet";
+
 import { ShareComponent } from "../share/share.component";
 import { HelpDialogService } from "../help-dialog/help-dialog.service";
+import { HomeServiceService } from "./home-service.service";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
 
-  constructor(private bottomSheet: MatBottomSheet,
-    private helpDialogService: HelpDialogService) {
+  constructor(
+    private bottomSheet: MatBottomSheet,
+    private helpDialogService: HelpDialogService,
+    private homeService: HomeServiceService) {
+  }
+
+  private destroySubject: Subject<any> = new Subject<any>();
+
+  ngOnDestroy(): void {
+    this.destroySubject.next();
+    this.destroySubject.complete();
+  }
+
+  public onChromeStore(url: string) {
+    this.homeService.onChromeStore(url);
+  }
+
+  public onEdgeStore(url: string) {
+    this.homeService.onEdgeStore(url);
   }
 
   public share(): void {
@@ -19,6 +43,11 @@ export class HomeComponent {
   }
 
   public help(): void {
-    this.helpDialogService.show();
+
+    this.homeService.onMobileDevices()
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe(() => {
+        this.helpDialogService.show();
+      });
   }
 }
