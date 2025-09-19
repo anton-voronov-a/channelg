2ca80c5e12c847ff14f16ffb73cd4927b35aabe8
user16 <user16@PC3>
2025-09-19 10:10:08 +0300

    embedding

--- docs/embedding-app/embedding.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    const iframe = document.getElementById('iframe');
-
-    const update = () => {
-        iframe.src = location.hash.replace('#', '');
-    }
-    update();
-    window.onpopstate = update;
-})()
